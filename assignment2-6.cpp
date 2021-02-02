b9198c3e08081a8df3ab5ed0126e982068e4a35f
zt6801 <zxtang0002@zonemail.clpccd.edu>
2021-02-02 07:14:24 +0000

    assignment2-6

---
+++ assignment2-6.cpp
@@ -0,0 +1,30 @@
+// Zhiqi Tang
+// Write a program that  stores the following values in five different variables: 28, 32, 37, 24, 33
+// The program should first calculate the sum of these five variables and store the result in a separate variable named sum. Then, the program should divide the sum variable by 5 to get the average.
+// Display the sum and average with precision 3 on the screen.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  double var1, var2, var3, var4, var5;
+  double sum;
+  double average;
+
+  var1 = 28;
+  var2 = 32;
+  var3 = 37;
+  var4 = 24;
+  var5 = 33;
+
+  sum = var1 + var2 + var3 + var4 + var5;
+  average = sum/5;
+
+  cout << "The sum is " << sum;
+  cout << " and the average is " << average;
+
+  return 0;
+
+}
