fab2448619581b66c3c373e4e27f17fa01787764
zt6801 <zxtang0002@zonemail.clpccd.edu>
2021-02-02 07:50:01 +0000

    assignment2-6.cpp

---
+++ assignment2-6.cpp
@@ -0,0 +1,27 @@
+// Zhiqi Tang
+
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+
+  double var1, var2, var3, var4, var5;
+  double sum;
+  double average;
+
+  var1 = 28;
+  var2 = 32;
+  var3 = 37;
+  var4 = 24;
+  var5 = 33;
+
+  sum = var1 + var2 + var3 + var4 + var5;
+  average = sum/5;
+
+  cout << "The sum is "<< sum; 
+  cout << " and the average is " << average;
+  
+  return 0;
+  
+  }
