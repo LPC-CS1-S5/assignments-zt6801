29bf4594456ef90ddcf97765ad14e78cad9f6941
Kyuwoong Lee <kwooler@gmail.com>
2021-02-03 20:41:27 +0000

    Q

--- assignment2-6.cpp
+++ assignment2-6.cpp
@@ -1,6 +1,7 @@
 // Zhiqi Tang
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 // Write a program that  stores the following values in five different variables: 28, 32, 37, 24, 33
 // The program should first calculate the sum of these five variables and store the result in a separate variable named sum. Then, the program should divide the sum variable by 5 to get the average.
 // Display the sum and average with precision 3 on the screen.
@@ -8,10 +9,13 @@
 >>>>>>> origin/main
 =======
 >>>>>>> origin/main
+=======
+>>>>>>> fab2448619581b66c3c373e4e27f17fa01787764
 
 #include <iostream>
 using namespace std;
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 int main()
@@ -21,6 +25,9 @@ int main ()
 =======
 int main () 
 >>>>>>> origin/main
+=======
+int main () 
+>>>>>>> fab2448619581b66c3c373e4e27f17fa01787764
 {
 
   double var1, var2, var3, var4, var5;
@@ -38,6 +45,7 @@ int main ()
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   cout << "The sum is " << sum;
   cout << " and the average is " << average;
 
@@ -47,14 +55,20 @@ int main ()
 =======
 =======
 >>>>>>> origin/main
+=======
+>>>>>>> fab2448619581b66c3c373e4e27f17fa01787764
   cout << "The sum is "<< sum; 
   cout << " and the average is " << average;
   
   return 0;
   
 <<<<<<< HEAD
+<<<<<<< HEAD
   }
 >>>>>>> origin/main
 =======
   }
 >>>>>>> origin/main
+=======
+  }
+>>>>>>> fab2448619581b66c3c373e4e27f17fa01787764
