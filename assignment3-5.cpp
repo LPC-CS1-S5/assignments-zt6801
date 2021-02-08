ca302a2cbc5c9820fc8080c2e92757168e9139e6
zt6801 <zxtang0002@zonemail.clpccd.edu>
2021-02-08 23:01:28 +0000

    Assignment 3-5 program challenge 5

--- assignment3-5.cpp
+++ assignment3-5.cpp
@@ -6,8 +6,39 @@
 // output format: two precision after the decimal point
 
 #include <iostream> 
+#include <cstdlib> 
+#include <iomanip>
 using namespace std;
+
 int main()
 {
-  
+
+  const int MIN_VALUE = 0;
+  const int MAX_VALUE = 99;
+
+  int num1, num2, num3;
+  float sum;
+  float average;
+
+  unsigned seed = time(0);
+
+  srand(seed);
+
+  num1 = (rand() % (MAX_VALUE - MIN_VALUE +1)) + MIN_VALUE;
+  num2 = (rand() % (MAX_VALUE - MIN_VALUE +1)) + MIN_VALUE;
+  num3 = (rand() % (MAX_VALUE - MIN_VALUE +1)) + MIN_VALUE;
+  cout << "The generated values are " << num1 << ", " << num2 << ", and " << num3 << ".";
+
+  sum = (num1 + num2 + num3);
+  average= (num1 + num2 + num3)/3.0;
+
+  cout << setprecision(2) << fixed;
+  cout << " The summation of the 3 random numbers is " << sum << ", and the average is " << average << ". " << endl; 
+
+  return 0;
+
 }
+
+  
+
+
