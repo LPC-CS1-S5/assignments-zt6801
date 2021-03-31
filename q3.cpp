f40966f8fd698bec0cc3b70aa68fd10b301fe49c
zt6801 <zxtang0002@zonemail.clpccd.edu>
2021-03-31 16:04:01 +0000

    whoops more than 10 min

--- q3.cpp
+++ q3.cpp
@@ -6,19 +6,49 @@ If so, the number will be written into the file "numbers.txt". */
 #include <iostream>
 #include <srand>
 #include <time>
+#include <fstream>
 using namespace std;
 
 int getRdnum(void);
-int isGreater(int);
+int isGreater(int N);
 
 int main()
 {
+  ofstream ofs;
 
+  ofs.open("numbers.txt");
+  if (ofs.fail())
+  {
+    cerr << "File open error";
+    exit(0);
+  }
+
+  int isGreater();
+
+  for (i = 0, i < N, i++)
+  {
+    getRdnum();
+    if (isGreater(num))
+    {
+      ofs >> num;
+    }
+  }
+  ofs.close();
 }
 
 int getRdnum()
 {
+  int num;
+  int MIN_VALUE = 1;
+      MAX_ VALUE = 50;
 
+  unsigned seed = time(0);
+  srand(seed);
+  num = (rand() % (MAX_VALUE - MIN_VALUE +1 ) + MIN_VALUE)
 }
 
-int isGreater ()
+int isGreater(int N)
+{
+  
+
+}
