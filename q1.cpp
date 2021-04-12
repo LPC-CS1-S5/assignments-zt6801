b09149b64d112814365c05ca035efb8a3e0974da
zt6801 <>
2021-04-12 18:45:33 +0000

    more than 10 min sorry! I was trying to debug :(

--- q1.cpp
+++ q1.cpp
@@ -1,37 +1,44 @@
 // Make a program that calls the two overloaded functions swapTwoValues() so that two values are swapped in the function. 
+
 #include <iostream>
-#include <iomanip>
+#include <iomanip>  // Use for setw(5)
 using namespace std;
 
-void swapTwoValues(int &, int &);
-void swapTwoValues(float &, float &);
+void swapTwoValues(int &, int &); // Function Prototype for swapTwoValues int
+void swapTwoValues(float &, float &); // Function Prototype for swapTwoValues float
 
 int main()
 {
-  int val1, val2;
+  int n1, n2;
+  float f1, f2; // Could use different variables, but I chose to keep the same to send to other functions because this is a quiz and I want it to look uniform
   cout << "Enter two values for integer swap: ";
-  cin >> val1 >> val2;
-  swapTwoValues(val1, val2);
-  cout << "Swapped Values: " << val1 << val2 << endl;
+  cin >> n1 >> n2;
+  cout << "Before Swap: " << n1 << setw(5) << n2 << endl; // Show before swap
+  swapTwoValues(n1, n2);  // Call function to get the swapped values
+  cout << "Swapped Values: " << n1 << setw(5) << n2 << endl;  // Show after swapped
 
-  cout << "Enter two values for the float swap: ";
-  cin >> val1 >> val2;
-  swapTwoValues(val1, val2);
-  cout << "Swapped Values: " << val1 << val2 << endl;
+  // Same for float :)
+  cout << "Enter two values for the float swap: ";  
+  cin >> f1 >> f2;
+  cout << "Before Swap: " << f1 << setw(5) <<  f2 << endl;
+  swapTwoValues(f1, f2);
+  cout << "Swapped Values: " << f1 << setw(5) << f2 << endl;
 
 }
-void swapTwoValues(int n1, int n2)
+void swapTwoValues(int &n1, int &n2)
 {
-  int tmp;
+  int tmp;  // insert temporary number to hold place of n1 so we can successfully swap the numbers
   tmp = n1;
   n1 = n2;
   n2 = tmp;
+  return; // Return the numbers
 }
 
-void swapTwoValues(float f1, float f2)
+void swapTwoValues(float &f1, float &f2)
 {
-  int tmp;
+  float tmp;  // Same as int swapTwoValues
   tmp = f1;
   f1 = f2;
   f2 = tmp;
+  return;
 }
