48493e451dde7fcc7960023dd2a54fc67e3c7b66
zt6801 <>
2021-05-03 22:59:51 +0000

    1

---
+++ q2.cpp
@@ -0,0 +1,29 @@
+// Print the left bottom triangle in 2D array
+
+#include <iostream>
+using namespace std;
+
+void printtriangle(int number[][3], int);
+
+int main()
+{
+  const int SIZE = 3;
+  int numbers[SIZE][SIZE] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+
+  printtriangle(numbers, SIZE);
+}
+
+void printtriangle(int num[][3], int size)
+{
+  for(int i = 0; i < size; i++)
+  {
+    for(int j = 0; j < size; j++)
+    {
+      if (i >= j)
+      {
+        cout << num[i][j];
+      }
+    }
+    cout << endl;
+  }
+}
