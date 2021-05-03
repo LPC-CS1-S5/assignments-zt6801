4b81fae8d1b1d804ea48189aa72e3cff309ff890
zt6801 <>
2021-05-03 23:30:23 +0000

    123

--- q3.cpp
+++ q3.cpp
@@ -1,6 +1,8 @@
 // Make the program that finds the greatest number in the first half and the second half in the array 
 
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 using namespace std;
 
 int findmax(int number[], int from, int to);
@@ -10,28 +12,36 @@ int main()
 {
   int number[N] = {10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
 
-  int from = 0, to = 0;
+  int from;
+  int to;
 
   findmax(number, from, to);
-  cout << from << to << endl;
+  cout << "The max in the first half is: " << from << endl;
+  cout << "The max in the second half is: " << to << endl;
 
 }
 
 int findmax(int num[], int from, int to)
 {
-  for(int i = 0; i < N/2; i++)
+  int max = 0;
+  int fmax = 0;
+  for(int i = 0; i <= N/2-1; i++)
   {
-    if (from < num[i])
+    if (max < num[i])
     {
-      from = num[i];
+      max = num[i];
     }
   }
-  for(int i = N/2; i <= num[N-1]; i++)
+  cout << max << endl;
+  from = max;
+  for(int i = N/2; i < N-1; i++)
   {
-    if(to < num[i])
+    if(fmax < num[i])
     {
-      to = num[i];
+      fmax = num[i];
     }
   }
+  cout << fmax << endl;
+  to = fmax;
   return from, to;
 }
