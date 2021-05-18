769cc04093394531968e2eaca348ddaadcf32e02
zt6801 <>
2021-05-18 04:44:13 +0000

    commit

--- q1.cpp
+++ q1.cpp
@@ -1,5 +1,6 @@
 // [1] Create the Structure for the information about an Employee.
 
+const int MAXSIZE = 1000;
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -12,3 +13,21 @@ struct Employee {
 };
 
 // [2] Create the text file for the employee information “employee.txt” 
+// [3] Make a function that constructs the structure Employee
+int makeStructArray(Employee[]);
+//[4] Make functions that find a particular employee with some conditions
+// Find the employee that has a salary greater than 100,000
+void richEmployee(Employee[], int);
+// Find the employee that works in the “Computer” department.
+void findComputer(Employee[], int);
+
+// [5] Make the main function that drives your functions.
+int main()
+{
+  Employee emp[MAXSIZE];
+  int numofEmployees = 0;
+
+  numofEmployees = makeStructArray(emp);
+
+
+}
