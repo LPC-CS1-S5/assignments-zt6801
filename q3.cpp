4be76a2a191b2adaa9d5b612ba8ed7eb1684d54a
zt6801 <>
2021-05-24 00:56:00 +0000

    ?

--- q3.cpp
+++ q3.cpp
@@ -1,37 +1,24 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
 
-const int SIZE1 = 5;
-const int SIZE2 = 10;
-int numbers1[SIZE1] = {2, 3, 5, 9, 10};
-int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 15, 17};
 
 int main()
 {
-  int subsettrue[SIZE1];
+  const int SIZE1 = 5;
+  const int SIZE2 = 10;
+  int numbers1[SIZE1] = {2, 3, 5, 9, 10};
+  int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 15, 17};
+  int inside[SIZE1];
 
-  for(int i = 0; i < SIZE2; i++)
+  for(int i = 0; i < SIZE1; i++)
   {
-    if (numbers1[i] == numbers2[i])
+    for(int j = 0; j < SIZE2; j++)
     {
-      subsettrue[i] = numbers1[i];
-    }
-    else
-    {
-      subsettrue[i] = 0;
+      strcmp(numbers1, numbers2)
     }
   }
 
-  for(int i = 0; i < SIZE2; i++)
-  {
-    if (subsettrue[i] != 0)
-    {
-      cout << "The array numbers1 is a subset of the array numbers2." << endl;
-    }
-    else
-    {
-      cout << "The array numbers1 is not a subset of the array numbers2." << endl;
-    }
-  }
+
 }
 
