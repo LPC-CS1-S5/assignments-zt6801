5231b6a656cdc72744aeacec51f0b26eb64ca2b0
zt6801 <>
2021-05-24 00:43:07 +0000

    ?

--- q3.cpp
+++ q3.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
+using namespace std;
+
 const int SIZE1 = 5;
 const int SIZE2 = 10;
 int numbers1[SIZE1] = {2, 3, 5, 9, 10};
 int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 15, 17};
+
+int main()
+{
+  for(int i = 0; i < SIZE2; i++)
+  {
+    if (numbers1[i] == numbers2[i])
+    {
+      cout << numbers1[i] << endl;
+    }
+  }
+}
 
