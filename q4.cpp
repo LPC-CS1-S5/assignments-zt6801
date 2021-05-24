dbcc29497b3459ec11097ec70a11b21c7a9d8dee
zt6801 <>
2021-05-24 01:25:04 +0000

    commit

--- q4.cpp
+++ q4.cpp
@@ -6,8 +6,26 @@ using namespace std;
 int main ()
 {
   string  userinput;
-    string  words= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini, qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
+  string  words= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini, qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
 
-    cout << "Enter user string "; 
-    cin >> userinput;
+  cout << "Enter user string "; 
+  cin >> userinput;
+
+  int N = words.length();
+  int M = userinput.length();
+  char latin[N];
+  char userinarray[M];
+  
+  strcpy(latin, words.c_str());
+  strcpy(userinarray, userinput.c_str());
+
+  char *pos;
+  pos = strstr(latin, userinarray);
+  if (pos)
+  {
+    cout << "found" << endl;
+  }
+
+
+
 }
